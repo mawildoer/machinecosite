81d84d72acf3acc9ba65897ef271a1714bcfd491
mawildoer <matthew.wildoer@geratech.com.au>
2017-12-06 00:17:29 +1100

    remove cnc machines for

--- typedTitle.js
+++ typedTitle.js
@@ -1,5 +1,5 @@
 var typed = new Typed(document.getElementById("typedSubtitle"),{
-  strings: ["CNC Machines for Prototyping","CNC Machines for Crafting Customs","CNC Machines for that One-off","CNC Machines for Designers","CNC Machines for Inventors","CNC Machines for Track Cars","CNC Machines for Modders","CNC Machines for Engineers","CNC Machines for Workshops","CNC Machines for Offices","CNC Machines for Universities"],
+  strings: ["For Prototyping","For Crafting Customs","For that One-off","For Designers","For Inventors","For Track Cars","For Modders","For Engineers","For Workshops","For Offices","For Universities"],
   typeSpeed: 50,
   backDelay : 2000,
   startDelay : 500,
