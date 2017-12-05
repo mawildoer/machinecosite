80ecd5d799063a5f2f7dd968be2d61bc2cd231c4
mawildoer <matthew.wildoer@geratech.com.au>
2017-12-06 00:10:05 +1100

    Adding new files

---
+++ typedTitle.js
@@ -0,0 +1,9 @@
+var typed = new Typed(document.getElementById("typedSubtitle"),{
+  strings: ["CNC Machines for Prototyping","CNC Machines for Crafting Customs","CNC Machines for that One-off","CNC Machines for Designers","CNC Machines for Inventors","CNC Machines for Track Cars","CNC Machines for Modders","CNC Machines for Engineers","CNC Machines for Workshops","CNC Machines for Offices","CNC Machines for Universities"],
+  typeSpeed: 50,
+  backDelay : 2000,
+  startDelay : 500,
+  loop : true,
+  backSpeed : 30,
+  showCursor : true
+});
